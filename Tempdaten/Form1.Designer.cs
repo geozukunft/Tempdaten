6ee8e8f3e9d3bc6dc39450b72da19b8478e30319
Viktor geozukunft <geozukunft@gmail.com>
2020-04-03 21:57:46 +0200

    final commit

--- Tempdaten/Form1.Designer.cs
+++ Tempdaten/Form1.Designer.cs
@@ -29,12 +29,205 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panelStatus = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.serialCommToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.portToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cboPorts = new System.Windows.Forms.ToolStripComboBox();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lboCelsius = new System.Windows.Forms.ListBox();
+            this.lboFahrenheit = new System.Windows.Forms.ListBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.cmdDeleteSelected = new System.Windows.Forms.Button();
+            this.cmdCalculate = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Gelb Fehler Verbindungsaufbau";
+            // 
+            // panelStatus
+            // 
+            this.panelStatus.Location = new System.Drawing.Point(191, 38);
+            this.panelStatus.Name = "panelStatus";
+            this.panelStatus.Size = new System.Drawing.Size(65, 56);
+            this.panelStatus.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Rot Verbindung getrennt";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Grün Verbindung aufgebaut";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.serialCommToolStripMenuItem,
+            this.closeApplicationToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // serialCommToolStripMenuItem
+            // 
+            this.serialCommToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.portToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.serialCommToolStripMenuItem.Name = "serialCommToolStripMenuItem";
+            this.serialCommToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.serialCommToolStripMenuItem.Text = "Serial Comm";
+            // 
+            // portToolStripMenuItem
+            // 
+            this.portToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cboPorts,
+            this.openToolStripMenuItem});
+            this.portToolStripMenuItem.Name = "portToolStripMenuItem";
+            this.portToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.portToolStripMenuItem.Text = "Port";
+            // 
+            // cboPorts
+            // 
+            this.cboPorts.Name = "cboPorts";
+            this.cboPorts.Size = new System.Drawing.Size(121, 23);
+            this.cboPorts.Click += new System.EventHandler(this.cboPorts_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // closeApplicationToolStripMenuItem
+            // 
+            this.closeApplicationToolStripMenuItem.Name = "closeApplicationToolStripMenuItem";
+            this.closeApplicationToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.closeApplicationToolStripMenuItem.Text = "Close Application";
+            this.closeApplicationToolStripMenuItem.Click += new System.EventHandler(this.closeApplicationToolStripMenuItem_Click);
+            // 
+            // lboCelsius
+            // 
+            this.lboCelsius.FormattingEnabled = true;
+            this.lboCelsius.Location = new System.Drawing.Point(18, 137);
+            this.lboCelsius.Name = "lboCelsius";
+            this.lboCelsius.Size = new System.Drawing.Size(148, 225);
+            this.lboCelsius.TabIndex = 13;
+            // 
+            // lboFahrenheit
+            // 
+            this.lboFahrenheit.FormattingEnabled = true;
+            this.lboFahrenheit.Location = new System.Drawing.Point(172, 137);
+            this.lboFahrenheit.Name = "lboFahrenheit";
+            this.lboFahrenheit.Size = new System.Drawing.Size(157, 225);
+            this.lboFahrenheit.TabIndex = 14;
+            // 
+            // cmdDeleteSelected
+            // 
+            this.cmdDeleteSelected.Location = new System.Drawing.Point(363, 137);
+            this.cmdDeleteSelected.Name = "cmdDeleteSelected";
+            this.cmdDeleteSelected.Size = new System.Drawing.Size(94, 27);
+            this.cmdDeleteSelected.TabIndex = 15;
+            this.cmdDeleteSelected.Text = "Delete Selected";
+            this.cmdDeleteSelected.UseVisualStyleBackColor = true;
+            this.cmdDeleteSelected.Click += new System.EventHandler(this.cmdDeleteSelected_Click);
+            // 
+            // cmdCalculate
+            // 
+            this.cmdCalculate.Location = new System.Drawing.Point(363, 171);
+            this.cmdCalculate.Name = "cmdCalculate";
+            this.cmdCalculate.Size = new System.Drawing.Size(94, 31);
+            this.cmdCalculate.TabIndex = 16;
+            this.cmdCalculate.Text = "Calculate";
+            this.cmdCalculate.UseVisualStyleBackColor = true;
+            this.cmdCalculate.Click += new System.EventHandler(this.cmdCalculate_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(363, 219);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(35, 13);
+            this.lblOutput.TabIndex = 17;
+            this.lblOutput.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.cmdCalculate);
+            this.Controls.Add(this.cmdDeleteSelected);
+            this.Controls.Add(this.lboFahrenheit);
+            this.Controls.Add(this.lboCelsius);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panelStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panelStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem serialCommToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem portToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox cboPorts;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeApplicationToolStripMenuItem;
+        private System.Windows.Forms.ListBox lboCelsius;
+        private System.Windows.Forms.ListBox lboFahrenheit;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button cmdDeleteSelected;
+        private System.Windows.Forms.Button cmdCalculate;
+        private System.Windows.Forms.Label lblOutput;
     }
 }
 
--- Tempdaten/Form1.cs
+++ Tempdaten/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
 
 namespace Tempdaten
 {
@@ -15,6 +17,147 @@ namespace Tempdaten
         public Form1()
         {
             InitializeComponent();
+            //Event for Serial Data recive
+
+
+            /*serialPort1.DataReceived += (sender, e) =>
+            {
+                string data = serialPort1.ReadLine().Trim();
+                lboCelsius.Items.Add(data);
+            };*/
+           
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e) //Serial Data Receive Event
+        {
+            
+            string data = serialPort1.ReadLine().Trim();
+            if(lboCelsius.Items.Count == 50)
+            {
+                lboCelsius.Items.Clear();
+                lboFahrenheit.Items.Clear();
+            }
+            lboCelsius.Items.Add(data);
+            double celsius = Convert.ToDouble(data);
+            double fahrenheit = (celsius * (9 / 5)) + 32;
+            lboFahrenheit.Items.Add(fahrenheit.ToString());
+           
+
+        }
+
+        //search all available Ports on Syttem
+        private void ListCom()
+        {
+            //Write all available ports into String
+            string[] ports = SerialPort.GetPortNames();
+
+            //Display available Ports
+            foreach (string port in ports)
+            {
+                cboPorts.Items.Add(port);
+            }
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (cboPorts.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select COM-POrt");
+            }
+            else
+            {
+                serialPort1.PortName = cboPorts.SelectedItem.ToString();
+                if (!serialPort1.IsOpen)
+                {
+                    try
+                    {
+                        serialPort1.Open();
+                        MessageBox.Show("Port opened");
+                        panelStatus.BackColor = Color.Green;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("A error occurred!");
+                        MessageBox.Show(ex.ToString());
+                        panelStatus.BackColor = Color.Yellow;
+                    }
+                }
+            }
+        }
+
+        private void closeApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cboPorts_Click(object sender, EventArgs e)
+        {
+            ListCom(); //List Ports in Menu
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                MessageBox.Show("Port closed");
+                panelStatus.BackColor = Color.Red;
+            }
+            else
+            {
+                MessageBox.Show("Port not open");
+            }
+        }
+
+        private void cmdDeleteSelected_Click(object sender, EventArgs e)
+        {
+            if(lboCelsius.SelectedIndex != -1)
+            {
+                lboCelsius.Items.Remove(lboCelsius.SelectedIndex);
+                lboFahrenheit.Items.Remove(lboCelsius.SelectedIndex);
+            }
+            else if(lboFahrenheit.SelectedIndex != -1)
+            {
+                lboCelsius.Items.Remove(lboFahrenheit.SelectedIndex);
+                lboFahrenheit.Items.Remove(lboFahrenheit.SelectedIndex);
+            }
+        }
+
+        private void cmdCalculate_Click(object sender, EventArgs e)
+        {
+            double ave = 0;
+            double min = Convert.ToDouble(lboCelsius.Items[0]);
+            double max = Convert.ToDouble(lboCelsius.Items[0]);
+            foreach (string s in lboCelsius.Items)
+            {
+                ave += Convert.ToDouble(s);
+                if(Convert.ToDouble(s)> max)
+                {
+                    max = Convert.ToDouble(s);
+                }
+                if (Convert.ToDouble(s) < min)
+                {
+                    min = Convert.ToDouble(s);
+                }
+            }
+            ave = ave / lboCelsius.Items.Count;
+
+            lblOutput.Text = "Average: " + Convert.ToString(ave) + "\n" +
+                "Max Value: " + Convert.ToString(max) + "\n" +
+                "Min Value: " + Convert.ToString(min) + "\n";
+
+            lboCelsius.Items.Clear();
+            lboFahrenheit.Items.Clear();
+
+
         }
     }
 }
